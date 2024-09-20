0b4c70795e5603c0dd55e4f7ab00243c5a913047
harishmahalingam03 <harish.m@atomedgesoft.com>
2024-09-20 13:07:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
+docker rm -f my-app || true
 docker network rm -f aura-network || true
 # Log in to AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
