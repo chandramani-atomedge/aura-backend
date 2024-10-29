e43f3b4ad3ac0b7b31b121cc52c8d755e868d51f
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-10-29 12:57:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,15 @@ docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest || tru
 
 docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
 
-NETWORK_NAME="aura-network"
+# NETWORK_NAME="aura-network"
 
-# Check if the network already exists
-if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
-    echo "Network '$NETWORK_NAME' already exists."
-else
-    # Create the network if it doesn't exist
-    docker network create "$NETWORK_NAME"
-    echo "Network '$NETWORK_NAME' created."
-fi
-# docker run -d --name my-app 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
+# # Check if the network already exists
+# if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
+#     echo "Network '$NETWORK_NAME' already exists."
+# else
+#     # Create the network if it doesn't exist
+#     docker network create "$NETWORK_NAME"
+#     echo "Network '$NETWORK_NAME' created."
+# fi
+# # docker run -d --name my-app 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
 #docker run --name aura-app --hostname localhost --network aura-network -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 8080:8080 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
