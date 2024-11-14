65c560811175f08d33b971a1783ff6af47ca6898
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-11-14 15:05:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,8 @@ sudo apt update -y
 # Log in to AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
 
-# docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest || true
+IMAGE_TAG=$(aws ssm get-parameter --name "docker-tag" --region ap-south-1 --query "Parameter.Value" --output text)
 
-docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:AURA-473B
+docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest || true
 
-# NETWORK_NAME="aura-network"
-
-# # Check if the network already exists
-# if docker network ls --format '{{.Name}}' | grep -w "$NETWORK_NAME" > /dev/null; then
-#     echo "Network '$NETWORK_NAME' already exists."
-# else
-#     # Create the network if it doesn't exist
-#     docker network create "$NETWORK_NAME"
-#     echo "Network '$NETWORK_NAME' created."
-# fi
-# # docker run -d --name my-app 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
-#docker run --name aura-app --hostname localhost --network aura-network -e "AWS_ACCESS_KEY_ID=AKIAT567PXHK6WVKNEOS3" -e "AWS_SECRET_ACCESS_KEY=9yZfLNgfLmMtIc+uERbDFw+jygll+MtjuX8rqWZKl" -e "OTP_API_KEY=5d7dbdd8-6498-11ef-8b60-0200cd936042" -p 8080:8080 -d 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest
+docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:$IMAGE_TAG
