f8c5ba65a01269452c1599de8367a5d80d087676
chandramani-atomedge <chandramani.r@atomedgesoft.com>
2024-11-14 15:07:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ sudo apt update -y
 # Log in to AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 270514764245.dkr.ecr.us-east-1.amazonaws.com
 
-IMAGE_TAG=$(aws ssm get-parameter --name "docker-tag" --region ap-south-1 --query "Parameter.Value" --output text)
+IMAGE_TAG=$(aws ssm get-parameter --name "backend-image-tag" --region ap-south-1 --query "Parameter.Value" --output text)
 
 docker rm -f 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-app:latest || true
 
